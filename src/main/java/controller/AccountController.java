50f6ff54a35ac5ac7bb410da8ea80f4d27e0be4e
truong-nh <104372670+truong-nh@users.noreply.github.com>
2022-11-11 22:32:25 +0700

    10:32

---
+++ src/main/java/controller/AccountController.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.user.User;
+
+/**
+ *
+ * @author Admin
+ */
+public class AccountController {
+     private User user;
+
+    public AccountController(User user) {
+        this.user = user;
+    }
+    
+}
---
+++ src/main/java/controller/FinanceController.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import model.user.User;
+
+/**
+ *
+ * @author Admin
+ */
+public class FinanceController {
+      private User user;
+
+    public FinanceController(User user) {
+        this.user = user;
+    }
+
+    
+}
