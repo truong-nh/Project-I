ee114010c92d6ea104280c7e4756f11bdcd4e4fd
begaupong2302 <86068524+begaupong2302@users.noreply.github.com>
2022-12-19 16:08:25 +0700

    .

--- Project-1/src/controller/TicketController.java
+++ Project-1/src/controller/TicketController.java
@@ -9,6 +9,7 @@ import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import model.book.Book;
 import model.ticket.BookRequestTicket;
 import model.ticket.BorrowTicket;
 import model.ticket.ExtendTicket;
@@ -103,9 +104,84 @@ public class TicketController {
     }
     
     //add ticket
-//    public void addBorrowTicket(int id, String status, Date dateCreate, ){
-//        BorrowTicket BorrowTicket = new BorrowTicket();
-//        BorrowTicket.setId();
-//        DBTicket.addBorrowTicket(BorrowTicket);
-//    }
+    public void addBorrowTicket(int id, String status, Date dateCreate, User borrower, Book book, Date borrowedDate, Date returnDate){
+        BorrowTicket BorrowTicket = new BorrowTicket();
+        BorrowTicket.setId(id);
+        BorrowTicket.setStatus(status);
+        BorrowTicket.setDateCreate(dateCreate);
+        BorrowTicket.setBorrower(borrower);
+        BorrowTicket.setBook(book);
+        BorrowTicket.setBorrowedDate(borrowedDate);
+        BorrowTicket.setReturnDate(returnDate);
+        DBTicket.addBorrowTicket(BorrowTicket);
+    }
+    public void addExtendTicket(int id, String status, Date dateCreate, Date newReturnDate, BorrowTicket borrowTicket){
+        ExtendTicket ExtendTicket = new ExtendTicket();
+        ExtendTicket.setId(id);
+        ExtendTicket.setStatus(status);
+        ExtendTicket.setDateCreate(dateCreate);
+        ExtendTicket.setBorrowTicket(borrowTicket);
+        ExtendTicket.setNewReturnDate(newReturnDate);
+        DBTicket.addExtendTicket(ExtendTicket);
+    }
+    public void addLendTicket(int id, String status, Date dateCreate, Date lendDate, BorrowTicket borrowTicket){
+        LendTicket LendTicket = new LendTicket();
+        LendTicket.setId(id);
+        LendTicket.setStatus(status);
+        LendTicket.setDateCreate(dateCreate);
+        LendTicket.setLendDate(lendDate);
+        LendTicket.setBorrowTicket(borrowTicket);
+        DBTicket.addLendTicket(LendTicket);
+    }
+    public void addPenaltyTicket(int id, String status, Date dateCreate, long penalty, BorrowTicket borrowTicket){
+        PenaltyTicket PenaltyTicket = new PenaltyTicket();
+        PenaltyTicket.setId(id);
+        PenaltyTicket.setStatus(status);
+        PenaltyTicket.setDateCreate(dateCreate);
+        PenaltyTicket.setPenalty(penalty);
+        PenaltyTicket.setBorrowTicket(borrowTicket);
+        DBTicket.addPenaltyTicket(PenaltyTicket);
+    }
+    
+    //update ticket
+    public boolean updateBorrowTicket(BorrowTicket BorrowTicket){
+        try {
+           DBTicket.updateBorrowTicket(BorrowTicket);
+           return true;
+       } catch (Exception e) {
+           return false;
+       }
+    } 
+    public boolean updateExtendTicket(ExtendTicket ExtendTicket){
+        try {
+           DBTicket.updateExtendTicket(ExtendTicket);
+           return true;
+       } catch (Exception e) {
+           return false;
+       }
+    }
+    public boolean updatePenaltyTicket(PenaltyTicket PenaltyTicket){
+        try {
+           DBTicket.updatePenaltyTicket(PenaltyTicket);
+           return true;
+       } catch (Exception e) {
+           return false;
+       }
+    }
+    public boolean updateLendTicket(LendTicket LendTicket){
+        try {
+           DBTicket.updateLendTicket(LendTicket);
+           return true;
+       } catch (Exception e) {
+           return false;
+       }
+    }
+    public boolean updateBookRequestTicket(BookRequestTicket BookRequestTicket){
+        try {
+           DBTicket.updateBookRequestTicket(BookRequestTicket);
+           return true;
+       } catch (Exception e) {
+           return false;
+       }
+    }
 }
